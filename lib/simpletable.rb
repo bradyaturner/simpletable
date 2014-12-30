bfc1e6dbb4e5e350b1abafe20582b534c8dc92fd
Brady Turner <bturner@nuvotechnologies.com>
2014-12-30 00:24:50 -0500

    Working version, sample code

--- lib/simpletable.rb
+++ lib/simpletable.rb
@@ -1,5 +1,35 @@
 require "simpletable/version"
 
+DEFAULT_DIVIDER = "="
+DEFAULT_PADDING = 2
+
 module SimpleTable
-  # Your code goes here...
+  def create( objects, titles, methods, options = {} )
+    raise "Mismatched number of methods and column titles" if titles.length != methods.length
+
+    divider = options[:divider] || DEFAULT_DIVIDER
+    padding = options[:padding] || DEFAULT_PADDING
+
+    widths = []
+    # calculate column widths
+    titles.zip(methods).each do |title,method|
+      widths << objects.collect { |o| o.send(method).to_s }.push(title).group_by(&:size).max.first + padding
+    end
+
+    # print table header
+    print_row(titles,widths)
+    puts divider * (widths.inject(:+) - padding)  # sum of column widths - padding
+
+    # print table body
+    objects.each do |o|
+      data = methods.collect{ |m| o.send(m) } # collect row data
+      print_row(data,widths)
+    end
+  end
+
+private
+  def print_row(data,widths)
+    data.zip(widths).each { |d,w| print d.ljust(w) }
+    print "\n"
+  end
 end
